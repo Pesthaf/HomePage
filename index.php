b04d96d751bb2ca237668c061e0d1d4a37ed8e47
Alexander Shalimov <tigrankryukov@gmail.com>
2019-01-06 17:13:03 +0300

    Fix yandex metrika

--- index.php
+++ index.php
@@ -26,6 +26,20 @@
 	<div class="counter"><?php require_once './lib/counter.php' ?> visitors of today</div>
   </footer>
 <!-- Yandex.Metrika counter -->
+<script>
+// Yandex.Metrika
+(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+ym(51709358, "init", {
+	id:51709358,
+	clickmap:true,
+	trackLinks:true,
+	accurateTrackBounce:true,
+	webvisor:true
+});
+</script>
 <noscript><div><img src="https://mc.yandex.ru/watch/51709358" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
 <!-- /Yandex.Metrika counter -->
 </body>
--- scripts.js
+++ scripts.js
@@ -7,17 +7,5 @@ window.addEventListener('resize', function () {
     window.location.reload(); 
 	console.log("Redrawing page...");
 });
-// Yandex.Metrika
-(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-ym(51709358, "init", {
-	id:51709358,
-	clickmap:true,
-	trackLinks:true,
-	accurateTrackBounce:true,
-	webvisor:true
-});
 
 console.log("Successful loading :3");
